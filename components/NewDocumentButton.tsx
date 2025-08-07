1402f74d58111911892aedae3b722e325a78e8ee
Aditya <adityapandey4254@gmail.com>
2025-08-08 02:13:27 +0530

    feature:Mydocuments and shared documents visibility

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -3,19 +3,14 @@ import { useRouter } from "next/navigation"
 import { Button } from "./ui/button"
 import { useTransition } from "react"
 import { createNewDocument } from "@/actions/actions"
-import { redirect } from "next/navigation"
 
 function NewDocumentButton() {
-  const [isPending, startTransition] = useTransition()
-  const router = useRouter()
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
   const handleCreateNewDocument = () => {
     startTransition(async () => {
-      const docId = await createNewDocument()
-      try {
-        redirect(`/doc/${docId}`)
-      } catch (error) {
-        console.error(error)
-      }
+      const { docId } = await createNewDocument();
+      // router.push(`/doc/${docId}`);
     })
   }
   return (
@@ -24,7 +19,7 @@ function NewDocumentButton() {
         {isPending ? "Creating..." : "New Document"}
       </Button>
     </div>
-  )
-}
+  );
+};
 
-export default NewDocumentButton
+export default NewDocumentButton;
